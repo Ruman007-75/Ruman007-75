97a5983ad3ca74f4db6901a52883b58319dfbe3c
Md Ruman Chowdhury <ruman4040@gmail.com>
2024-07-05 20:51:30 +0600

    class object
    Signed-off-by: Md Ruman Chowdhury <ruman4040@gmail.com>

---
+++ oop/class_object2.dart
@@ -0,0 +1,19 @@
+import 'person2.dart';
+
+void main() {
+  // //instance/object: className object-name = classname()
+  // Monitor myMonitor = Monitor('LG', 'MK600');
+  // print(myMonitor.companyName);
+  // print(myMonitor.model);
+  //
+  // Monitor sujonMonitor = Monitor('samsung', 'Mk700');
+  // //sujonMonitor.model='MK500';
+  // //print(sujonMonitor.model);
+  // sujonMonitor.printModelName();
+  // sujonMonitor.companyName= 'MSI';
+  // print(sujonMonitor.companyName);
+  // print(Monitor.sample);
+  // print (Monitor.getSampleDate());
+  Person Ruman = Person(name: 'Ruman', address: 'Sylhet', age: 34);
+  Ruman.printName();
+}
