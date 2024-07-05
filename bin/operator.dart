e577f6cb989da5f1482cf04f854599da912b839a
Md Ruman Chowdhury <ruman4040@gmail.com>
2024-07-05 23:48:59 +0600

    class object
    Signed-off-by: Md Ruman Chowdhury <ruman4040@gmail.com>

---
+++ bin/operator.dart
@@ -0,0 +1,26 @@
+main() {
+  int a = 3;
+  a = a + 1;
+  print(a);
+  a += 1;
+  a++; // ai 3 babe sum koraa jay
+  // a++ mne postfix
+  print(a);
+  print(++a); // prefix
+  print(a > 5);
+  print(a < 5);
+  print(a == 7);
+  print(a == 7 && a > 6);
+  print(a == 7 && a < 6);
+  print(a == 7 || a < 6);
+  print(33 % 2); // bag ses dekabe
+  print(33 / 2);
+  print(a--);
+  print(a);
+  print(--a);
+
+  a *= 18; // a= a *18
+  print(a);
+  // a /=5; jodi bag hoi tahole Double type dite hobe
+  // print(a);
+}
