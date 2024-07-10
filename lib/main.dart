fc5764175146927a798e921bf2c4e144fd3c7381
Md Ruman Chowdhury <ruman4040@gmail.com>
2024-07-10 08:54:43 +0600

    ostad_batch_seven_first_class

--- lib/main.dart
+++ lib/main.dart
@@ -18,69 +18,26 @@ class IntroApp extends StatelessWidget {
 class Home extends StatelessWidget {
   const Home({super.key});
 
+// child, children(multiful)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.white,
-      ),
-      body: Center(
-        // child: Icon(
-        //   Icons.android,
-        //   size: 50,
-        //   color: Colors.white,
-        //child: Image.network('https://www.google.com/url?sa=i&url=http%3A%2F%2Ft3.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcSwmCPjmbBPJRYncnUkl1BroxMBk-Zzx2gWBiDd8dq2RgRtZ4_irCXfS51bNYF0TL9t&psig=AOvVaw1lWVYIAJQIC1k8aTDaiqLE&ust=1720247795609000&source=images&cd=vfe&opi=89978449&ved=0CAkQjRxqFwoTCIDrksekj4cDFQAAAAAdAAAAABAE'),
-        // child: Image.asset('assets/image/shaky.jpg',
-        //   // width: 400,
-        //   // height: 200,
-        // fit: BoxFit.cover),
-        // child: ElevatedButton(
-        //   onPressed: () {
-        //     print('on pressed');
-        //   },
-        // child: Text('Notification'),
-        // ),
-        // child: IconButton(onPressed: () {
-        //   print('ADD');
-        // },
-        // icon: Icon(Icons.add),),
-
-        // child: TextButton(onPressed: () {
-        //   print('object');
-        // },
-        // child: Text('Ruman chy'),),
-
-        // child: GestureDetector(
-        //   onTap: (){
-        //     print('Single Tab');
-        //   },
-        //   onDoubleTap: (){
-        //     print('Double tab');
-        //   },
-        //   child: Text('Normal text'),
-
-        // uporer gestureDetector niche Inkwell aktu defference
-
-        child: InkWell(
-        onTap: (){
-        print('Single Tab');
-        },
-        onDoubleTap: (){
-        print('Double tab');
-        },
-        child: Text('Normal text'),
-
-
-    // j0di image na teke normal text take tahole
-          // child: Image.asset('assets/image/shaky.jpg',
-          //   width: 400,
-          //   height: 200,
-          // fit: BoxFit.cover),
+        appBar: AppBar(
+          title: Text('Home'),
+          backgroundColor: Colors.white,
         ),
-
-        ),
-      );
-
+        body: Column(
+          // vertically arrange kore
+          children: [
+            Text('hellow'),
+            Text('Ruman'),
+            Text('Jayed'),
+            Text('Harun'),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text('Click here')),
+            IconButton(onPressed: (){}, icon: Icon(Icons.access_time))
+          ],
+        ));
   }
 }
