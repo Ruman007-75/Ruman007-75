42c2682ea097fb818e81c7bdc7fb1248b20f1000
Md Ruman Chowdhury <ruman4040@gmail.com>
2024-08-30 19:41:27 +0600

    module11_class3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -29,36 +28,29 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    MediaQueryData mediaQuery = MediaQuery.of(context);
-    print(mediaQuery.size.width);
-    print(mediaQuery.size.height);
-    print(mediaQuery.size.aspectRatio);
-    print(mediaQuery.size.flipped.height);
-    print(mediaQuery.size);
-    if(mediaQuery.size.width<640){
-      print('This is a phone');
-    } else if(mediaQuery.size.width<640 && mediaQuery.size.width>1008){
-      print('This is a tablet');
-    } else {
-      print('LAPTOP/DESKTOP');
-    }
+    //MediaQueryData mediaQuery = MediaQuery.of(context);
+     print(MediaQuery.sizeOf(context).width);
+    // print(mediaQuery.size.height);
+    // print(mediaQuery.size.aspectRatio);
+    // print(mediaQuery.size.flipped.height);
+    // print(mediaQuery.size);
+    // if(mediaQuery.size.width<640){
+    //   print('This is a phone');
+    // } else if(mediaQuery.size.width<640 && mediaQuery.size.width>1008){
+    //   print('This is a tablet');
+    // } else {
+    //   print('LAPTOP/DESKTOP');
+    // }
 
     return Scaffold(
     appBar: AppBar(
       title: Text('Home'),
     ),
-      body: Container(
-        height: MediaQuery.of(context).size.width,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.yellow,
-        child: FractionallySizedBox(
-          widthFactor: 0.5,
-          heightFactor: 0.5,
-          child: Container(
-            color: Colors.green,
-          ),
-        ),
-      ),
+
+      body: LayoutBuilder(builder: (context, constrains){
+        return Text(constrains.maxWidth.toString());
+      }),
+      // vitore ba bahire same kaj korbe layoutBuilder
     );
   }
 }
