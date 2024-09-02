413f6fd053f9a68a2f8b1cb8ac31531f909098f8
Md Ruman Chowdhury <ruman4040@gmail.com>
2024-09-02 21:30:27 +0600

    responsive_design

--- lib/prsentstion/screen/home_screen.dart
+++ lib/prsentstion/screen/home_screen.dart
@@ -23,46 +23,85 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildMobileLayout() {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Firstwab\n #MP',
-          style: TextStyle(color: Colors.black, fontSize: 18),
-        ),
-        backgroundColor: Colors.white30,
-      ),
       drawer: const NavMenu(),
       body: Column(
-        children: [DeshBoardCard()],
+        children: [DeshBoardCard(),
+        Column(
+          children: [Text('FLUTTER WAB\n THE BASIC', style: TextStyle(color: Colors.black, fontSize: 30),),
+          SizedBox(height: 7,),
+            Text('In publishing and graphic design, Lorem ipsum \n'
+                ' ipsum is a placeholder text commonly used doc \n'
+                'to demonstrate the visual form of a document or\n'
+                ' without relying on meaningful content.', style: TextStyle(color: Colors.black, fontSize: 16),),
+            SizedBox(height: 20,),
+            ElevatedButton(style:ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: (){}, child: Text('TextButton', style: TextStyle(color: Colors.white),),
+            )
+          ],
+        )
+        ],
       ),
     );
   }
 
   Widget _buildTabletLayout() {
-    return const Scaffold(
+    return  Scaffold(
       body: Row(
         children: [
           NavMenu(),
           Expanded(
               child: Center(
             child: Text('FLUTTER WAB\n THE BASIC'),
-          ))
+          )),
+          Column(
+            children: [Text('FLUTTER WAB\n THE BASIC', style: TextStyle(color: Colors.black, fontSize: 30),),
+              SizedBox(height: 7,),
+              Text('In publishing and graphic design, Lorem ipsum \n'
+                  ' ipsum is a placeholder text commonly used doc \n'
+                  'to demonstrate the visual form of a document or\n'
+                  ' without relying on meaningful content.', style: TextStyle(color: Colors.black, fontSize: 16),),
+              SizedBox(height: 20,),
+              ElevatedButton(style:ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: (){}, child: Text('TextButton', style: TextStyle(color: Colors.white),),
+              )
+            ],
+          ),
+
         ],
       ),
     );
   }
 
   Widget _buildDesktopLayout() {
-    return const Scaffold(
+    return Scaffold(
       body: Row(
         children: [
           NavMenu(),
           Expanded(
               child: Center(
             child: Text('FLUTTER WAB\n THE BASIC'),
-          ))
-        ],
+          )),
+          Center(
+            child: Column(
+              children: [Text('FLUTTER WAB\n THE BASIC', style: TextStyle(color: Colors.black, fontSize: 30),),
+                    SizedBox(height: 7,),
+                    Text('In publishing and graphic design, Lorem ipsum \n'
+              ' ipsum is a placeholder text commonly used doc \n'
+              'to demonstrate the visual form of a document or\n'
+              ' without relying on meaningful content.', style: TextStyle(color: Colors.black, fontSize: 16),),
+                    ],
+            ),
+          ),
+            SizedBox(height: 20,),
+            ElevatedButton(style:ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              onPressed: (){}, child: Text('TextButton', style: TextStyle(color: Colors.white),),
+            )
+          ]
+
       ),
+
     );
   }
 
   }
+
